bb3e71af8627c2e0689ed96723d8fb7a3acfcef6
aigounad <abdellatifigounad@gmail.com>
2025-12-27 15:40:56 +0100

    Add PATCH method to CORS configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -30,7 +30,7 @@ export const createApp = () => {
         ? env.CLIENT_URL
         : ['http://localhost:5173', 'http://localhost:3000'],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   };
 
